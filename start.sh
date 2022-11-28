7fd299aa55b1a76f2ec161d9249f667721ec1cf3
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-11-28 22:05:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jnanesh12/jnaneshkoyeb /EvaMaria
+  git clone https://github.com/Jnanesh124/jnaneshkoyeb7 /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
